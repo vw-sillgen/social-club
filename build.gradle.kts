365bb35833f99c066608cc2a3ff55fa0efc599f3
Stefan Illgen <stefan.illgen@volkswagen.de>
2023-11-10 13:03:05 +0100

    introduce gradle build tool

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+allprojects {
+  group = "org.club.social"
+  version = "0.0.1-SNAPSHOT"
+
+  apply(plugin = "base")
+
+  repositories {
+    mavenCentral()
+  }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "social-club"
+//include("service")
+//include("ui")
